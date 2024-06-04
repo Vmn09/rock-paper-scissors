2b446a30a0600354f3b7614004e72dd3d97f7ec4
Vmn09 <amonsv01@gmail.com>
2024-06-04 15:48:31 +0200

    Edited code to use binary, changed gameplay system to 5 wins instead of 5 rounds.

--- script.js
+++ script.js
@@ -1,70 +1,97 @@
+let rounds = 0;
+let computerWins = 0;
+let humanWins = 0;
+
+const outcome = document.querySelector("#final")
+const finalValue = document.createElement("span")
+const matchData = document.querySelector("#data")
+const round = document.createElement("span")
+const playerScore = document.createElement("span")
+const computerScore = document.createElement("span")
+
+matchData.appendChild(round)
+matchData.appendChild(playerScore)
+matchData.appendChild(computerScore)
+
 function getComputerChoice() {
     let randNum = Math.random()
     switch(true) {
         case (randNum < (1/3)):
-            return "rock";
+            return "100";
             break;
         case ((1/3) < randNum && randNum < (2/3)):
-            return "paper"
+            return "010"
             break;
         case ((2/3) < randNum):
-            return "scissors";
+            return "001";
             break;
-        default:
-            "Bozo"
     }
 };
 
-function getHumanChoice() {
-    let val = prompt("Rock, Paper or Scissors?" ,"").toLowerCase()
-    return val
-};
 
-let computerScore = 0;
-let humanScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "Draw!";
-    } else if (humanChoice === "rock") {
-        if (computerChoice === "paper") {
-            computerScore++
-            return "You lost!";
+let button = document.querySelector("#buttons");
+button.addEventListener("click", (event) => {sessionStorage
+    if (computerWins < 5 && humanWins < 5) {
+        const win = [10, 20, 33];
+        const lose = [12, 17, 34];
+        let target = event.target;
+        let value = toBinary(target.id);
+        if (win.includes(value)) {
+            humanWins++
+            console.log("Win");
+        } else if (lose.includes(value)) {
+            computerWins++
+            console.log("Lose")
         } else {
-            humanScore++
-            return "You won!";
+            console.log("Draw")
+        };
+        if (computerWins == 5 || humanWins == 5) {
+            console.log("OVER")
+            final(humanWins)
         }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
-            computerScore++
-            return "You lost!";
+        rounds++
+        console.log("Rounds " +rounds)
+        console.log("Human " +humanWins)
+        console.log("Bot " +computerWins)
         } else {
-            humanScore++
-            return "You won!";
+            rounds = 0;
+            computerWins = 0;
+            humanWins = 0;
+            finalValue.textContent = ""
         }
-    } else if (humanChoice === "scissors") {
-        if (computerChoice === "rock") {
-            computerScore++
-            return "You lost!";
-        } else {
-            humanScore++
-            return "You won!";
-        }
-    };
-};
+        round.textContent = `Round ${rounds}\n`
+        playerScore.textContent = `Player score: ${humanWins}\n`
+        computerScore.textContent = `Computer score: ${computerWins}\n`
 
-let hV = getHumanChoice();
-let cV = getComputerChoice();
+}
+);
 
-
-
-function playGame() {
-    for (let i = 0; i==5; i++) {
-        playRound(hV, cV);
-        console.log(playRound(hV, cV));
-        console.log(computerScore);
-        console.log(humanScore);
+function toBinary(hC) {
+    console.log(hC)
+    let cC = getComputerChoice()
+    console.log(cC)
+    let binVal = ''
+    switch(hC) {
+        case 'rock':
+            binVal = `100${cC}`;
+            break;
+        case 'paper':
+            binVal = `010${cC}`;
+            break;
+        case 'scissors':
+            binVal = `001${cC}`;
+            break;
     }
+    let decVal = parseInt(binVal, 2);
+    console.log(decVal)
+    return decVal;
 }
 
-playGame()
+function final(humanWins) {
+    if (humanWins == 5) {
+        finalValue.textContent = "You won!"
+    } else {
+        finalValue.textContent = "You lost!"
+    }
+    outcome.appendChild(finalValue)
+}
